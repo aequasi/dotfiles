643efffc2281d01a982c6ad64018b1a4d1577b08
Aaron Scherer <aequasi@gmail.com>
2014-02-27 15:15:21 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
     sudo apt-get update -y
     sudo apt-get upgrade -y
-    sudo apt-get install -y vim rake zsh ruby-bundler
+    sudo apt-get install -y git vim rake zsh ruby-bundler
     git clone https://github.com/aequasi/dotfiles.git -b zsh "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" == "ask" ] && export ASK="true"
