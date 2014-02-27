b6ee6b891e90f7550406139b481cf5d35fd84498
Aaron Scherer <aequasi@gmail.com>
2014-02-26 23:14:01 -0800

    Updating install

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    apt-get install -y vim rake zsh
+    git clone https://github.com/aequasi/dotfiles.git -b zsh "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" == "ask" ] && export ASK="true"
     rake install
